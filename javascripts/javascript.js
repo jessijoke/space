bcb18c9fd73684cf4ab1122e6464e5eb4e08f218
jessijoke <jessijoke@yahoo.com>
2021-04-29 22:13:30 -0400

    Trying this out

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,9 +1,22 @@
 if (window.DeviceOrientationEvent && 'ontouchstart' in window) {
-    alert("This is going to work");
+    //alert("This is going to work");
     var gn = new GyroNorm();
 
     gn.init().then(function(){
     gn.start(function(data){
+        
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = data.dm.x;
+        let _mouseY = data.dm.y;
+        let _depth1 = `${50 - (_mouseX - _w) * 0.000001}% ${50 - (_mouseY - _h) * 0.000001}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.00002}% ${50 - (_mouseY - _h) * 0.00002}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.0005}% ${50 - (_mouseY - _h) * 0.0005}%`;
+        let _depth4 = `${50 - (_mouseX - _w) * 0.007}% ${50 - (_mouseY - _h) * 0.007}%`;
+        let _depth5 = `${50 - (_mouseX - _w) * 0.03}% ${50 - (_mouseY - _h) * 0.03}%`;
+        let x = `${_depth5}, ${_depth4}, ${_depth3}, ${_depth2}, ${_depth1}`;
+        console.log(x);
+        elem.style.backgroundPosition = x;
         
     });
     }).catch(function(e){
