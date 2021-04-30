48ea0b9f833be09048819e37c80b1fc0d71f870a
jessijoke <jessijoke@yahoo.com>
2021-04-29 22:06:25 -0400

    Test

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -1,37 +1,47 @@
-(function() {
-    // Add event listener
-    document.addEventListener("mousemove", space);
-    const elem = document.querySelector("#space");
-    // Magic happens here
-    function space(e) {
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _mouseX = e.clientX;
-        let _mouseY = e.clientY;
-        let _depth1 = `${50 - (_mouseX - _w) * 0.000001}% ${50 - (_mouseY - _h) * 0.000001}%`;
-        let _depth2 = `${50 - (_mouseX - _w) * 0.00002}% ${50 - (_mouseY - _h) * 0.00002}%`;
-        let _depth3 = `${50 - (_mouseX - _w) * 0.0005}% ${50 - (_mouseY - _h) * 0.0005}%`;
-        let _depth4 = `${50 - (_mouseX - _w) * 0.007}% ${50 - (_mouseY - _h) * 0.007}%`;
-        let _depth5 = `${50 - (_mouseX - _w) * 0.03}% ${50 - (_mouseY - _h) * 0.03}%`;
-        let x = `${_depth5}, ${_depth4}, ${_depth3}, ${_depth2}, ${_depth1}`;
-        console.log(x);
-        elem.style.backgroundPosition = x;
-    }
+if (window.DeviceOrientationEvent && 'ontouchstart' in window) {
+    var gn = new GyroNorm();
 
-})();
-
-var gn = new GyroNorm();
-
-gn.init().then(function(){
+    gn.init().then(function(){
     gn.start(function(data){
+        alert(data.dm.x);
+    });
+    }).catch(function(e){
+    // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
+    });
+} else {
+    // setup some mouse following hack
+    (function() {
+        // Add event listener
+        document.addEventListener("mousemove", space);
+        const elem = document.querySelector("#space");
+        // Magic happens here
+        function space(e) {
+            let _w = window.innerWidth/2;
+            let _h = window.innerHeight/2;
+            let _mouseX = e.clientX;
+            let _mouseY = e.clientY;
+            let _depth1 = `${50 - (_mouseX - _w) * 0.000001}% ${50 - (_mouseY - _h) * 0.000001}%`;
+            let _depth2 = `${50 - (_mouseX - _w) * 0.00002}% ${50 - (_mouseY - _h) * 0.00002}%`;
+            let _depth3 = `${50 - (_mouseX - _w) * 0.0005}% ${50 - (_mouseY - _h) * 0.0005}%`;
+            let _depth4 = `${50 - (_mouseX - _w) * 0.007}% ${50 - (_mouseY - _h) * 0.007}%`;
+            let _depth5 = `${50 - (_mouseX - _w) * 0.03}% ${50 - (_mouseY - _h) * 0.03}%`;
+            let x = `${_depth5}, ${_depth4}, ${_depth3}, ${_depth2}, ${_depth1}`;
+            console.log(x);
+            elem.style.backgroundPosition = x;
+        }
+    
+    })();
+    
+}
+
+
+
         // Process:
     // data.do.alpha	( deviceorientation event alpha value )
     // data.do.beta		( deviceorientation event beta value )
     // data.do.gamma	( deviceorientation event gamma value )
     // data.do.absolute	( deviceorientation event absolute value )
-
-    console.log(data.dm.x);
-    // data.dm.x		( devicemotion event acceleration x value )
+     // data.dm.x		( devicemotion event acceleration x value )
     // data.dm.y		( devicemotion event acceleration y value )
     // data.dm.z		( devicemotion event acceleration z value )
 
@@ -42,7 +52,3 @@ gn.init().then(function(){
     // data.dm.alpha	( devicemotion event rotationRate alpha value )
     // data.dm.beta		( devicemotion event rotationRate beta value )
     // data.dm.gamma	( devicemotion event rotationRate gamma value )
-    });
-}).catch(function(e){
-  // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-});
