a005bf968455a6e1893fbc594fa2016038efb408
jessijoke <jessijoke@yahoo.com>
2021-05-02 23:50:41 -0400

    again tweaking the math for mobile

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -16,8 +16,8 @@ if (window.DeviceOrientationEvent && 'ontouchstart' in window) {
         //let _mouseY = data.dm.y;
         let _depth1 = `50% 50%`;
         let _depth2 = `${50 - (xTilt)}% ${50 - (yTilt)}%`;
-        let _depth3 = `${50 - ((xTilt - 40) * .00005)}% ${50 - ((xTilt - 40) * .00005)}%`;
-        let _depth4 = `${50 - ((xTilt - 40) * .005)}% ${50 - ((xTilt - 40) * .005)}%`;
+        let _depth3 = `${50 - ((xTilt - 40) * .0000000005)}% ${50 - ((xTilt - 40) * .0000000005)}%`;
+        let _depth4 = `${50 - ((xTilt - 40) * .00005)}% ${50 - ((xTilt - 40) * .00005)}%`;
         let _depth5 = `${50 - ((xTilt - 30) * 1)}% ${50 - ((xTilt - 30) * 1)}%`;
         let x = `${_depth5}, ${_depth4}, ${_depth3}, ${_depth2}, ${_depth1}`;
         //console.log(x);
