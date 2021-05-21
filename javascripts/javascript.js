2e6bc1dbc7c00bada0d57b88d8d4b2e041c89e9e
jessijoke <jessijoke@yahoo.com>
2021-05-21 17:47:54 -0400

    Small update

--- javascripts/javascript.js
+++ javascripts/javascript.js
@@ -2,27 +2,34 @@ const elem = document.querySelector("#space");
 const displayText = document.querySelector("#text");
 
 if (window.DeviceOrientationEvent && 'ontouchstart' in window) {
-    
-    window.addEventListener("deviceorientation", handleOrientation, true);
+    function getAccel(){
+        DeviceMotionEvent.requestPermission().then(response => {
+            if (response == 'granted') {
+                console.log("accelerometer permission granted");
+                window.addEventListener("deviceorientation", handleOrientation, true);
 
-    function handleOrientation(event) {
-        let xTilt = event.alpha;
-        let yTilt = event.gamma;
+                function handleOrientation(event) {
+                    let xTilt = event.beta;
+                    let yTilt = event.gamma;
 
-        let _w = window.innerWidth/2;
-        let _h = window.innerHeight/2;
-        let _depth1 = `50% 50%`;
-        let _depth2 = `${10000 - ((yTilt + 1000) / .1000)}% ${10000 - ((xTilt + 1000) / .1000)}%`;
-        let _depth3 = `${1000 - ((yTilt + 100) / .1000)}% ${1000 - ((xTilt + 100) / .1000)}%`;
-        let _depth4 = `${100 - ((yTilt + 10) / .1000)}% ${100 - ((xTilt + 10) / .1000)}%`;
-        let _depth5 = `${50 - ((yTilt + 1) / .1000)}% ${50 - ((xTilt + 1) / .1000)}%`;
-        let x = `${_depth5}, ${_depth4}, ${_depth3}, ${_depth2}, ${_depth1}`;
-        //console.log(x);
-        elem.style.backgroundPosition = x;
+                    let _w = window.innerWidth/2;
+                    let _h = window.innerHeight/2;
+                    let _depth1 = `50% 50%`;
+                    let _depth2 = `${10000 - ((yTilt + 1000) / .1000)}% ${10000 - ((xTilt + 1000) / .1000)}%`;
+                    let _depth3 = `${1000 - ((yTilt + 100) / .1000)}% ${1000 - ((xTilt + 100) / .1000)}%`;
+                    let _depth4 = `${100 - ((yTilt + 10) / .1000)}% ${100 - ((xTilt + 10) / .1000)}%`;
+                    let _depth5 = `${50 - ((yTilt + 1) / .1000)}% ${50 - ((xTilt + 1) / .1000)}%`;
+                    let x = `${_depth5}, ${_depth4}, ${_depth3}, ${_depth2}, ${_depth1}`;
+                    //console.log(x);
+                    elem.style.backgroundPosition = x;
+                }
+
+                setTimeout(handleOrientation(), 100);
+
+            }
+        });
     }
-
-    setTimeout(handleOrientation(), 100);
-
+    
 
 } else {
     // setup some mouse following hack
@@ -35,11 +42,11 @@ if (window.DeviceOrientationEvent && 'ontouchstart' in window) {
             let _h = window.innerHeight/2;
             let _mouseX = e.clientX;
             let _mouseY = e.clientY;
-            let _depth1 = `${50 - (_mouseX - _w) * 0.000001}% ${50 - (_mouseY - _h) * 0.000001}%`;
-            let _depth2 = `${50 - (_mouseX - _w) * 0.00002}% ${50 - (_mouseY - _h) * 0.00002}%`;
-            let _depth3 = `${50 - (_mouseX - _w) * 0.0005}% ${50 - (_mouseY - _h) * 0.0005}%`;
-            let _depth4 = `${50 - (_mouseX - _w) * 0.007}% ${50 - (_mouseY - _h) * 0.007}%`;
-            let _depth5 = `${50 - (_mouseX - _w) * 0.03}% ${50 - (_mouseY - _h) * 0.03}%`;
+            let _depth1 = `${50 - (_mouseX - _w) * 0.00001}% ${50 - (_mouseY - _h) * 0.00001}%`;
+            let _depth2 = `${50 - (_mouseX - _w) * 0.0002}% ${50 - (_mouseY - _h) * 0.0002}%`;
+            let _depth3 = `${50 - (_mouseX - _w) * 0.025}% ${50 - (_mouseY - _h) * 0.025}%`;
+            let _depth4 = `${50 - (_mouseX - _w) * 0.07}% ${50 - (_mouseY - _h) * 0.07}%`;
+            let _depth5 = `${50 - (_mouseX - _w) * 0.3}% ${50 - (_mouseY - _h) * 0.3}%`;
             let x = `${_depth5}, ${_depth4}, ${_depth3}, ${_depth2}, ${_depth1}`;
             //console.log(x);
             elem.style.backgroundPosition = x;
